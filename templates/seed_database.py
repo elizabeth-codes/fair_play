c936fb9cc61f62fe963712bf7942770aacf8cc3c
Elizabeth Joyce <ekj28@cornell.edu>
2023-06-20 00:39:53 -0400

    remove old files

--- templates/seed_database.py
+++
@@ -1,9 +0,0 @@
-#Create 10 users
-for n in range(10):
-    email = f"user{n}@test.com"
-    password = "test"
-
-    user = crud.create_user(email, password)
-    model.db.session.add(user)
-
-model.db.session.commit()
